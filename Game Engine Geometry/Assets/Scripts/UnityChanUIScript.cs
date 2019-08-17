fa90392b15b04e802169a52ddeaea7c058411db7
Cade Anderson <cadeanderson99@gmail.com>
2019-08-16 17:55:58 -0700

    Add Unity-Chan assets

---
+++ Game Engine Geometry/Assets/Scripts/UnityChanUIScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UnityChanUIScript : MonoBehaviour
+{
+    float movementSpeed;
+    [SerializeField]
+    Animator unityChanAnimator = default;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void OnSliderValueChanged(Slider slider)
+    {
+        movementSpeed = slider.value;
+        unityChanAnimator.SetFloat("Move Speed", slider.value);
+    }
+
+}
---
+++ Game Engine Geometry/Assets/unity-chan!/Unity-chan! Model/SplashScreen/Scripts/SplashScreen.cs
@@ -0,0 +1,16 @@
+﻿#pragma warning disable 0618
+using UnityEngine;
+using System.Collections;
+
+namespace UnityChan
+{
+	[ExecuteInEditMode]
+	public class SplashScreen : MonoBehaviour
+	{
+		void NextLevel ()
+		{
+			Application.LoadLevel (Application.loadedLevel + 1);
+		}
+	}
+}
+#pragma warning restore 0618
